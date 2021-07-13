c4c6005fb1fc12f401f2b68584424707449ea9d2
ZaynAkira <87105213+ZaynAkira@users.noreply.github.com>
2021-07-13 15:03:09 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord, os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=["c.", "C."], case_insensitive=True, intents=discord.Intents.all())
+
+for namafile in os.listdir('./cogs'):
+    if namafile.endswith('.py'):
+        client.load_extension(f'cogs.{namafile[:-3]}')
+        print(f"{namafile} dimuat")
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('c.help'))
+    print(f"\n{client.user} sudah siap.")
+
+    channel = client.get_channel(847885141019983962)
+    await channel.send(f"**{client.user}** telah diaktifkan kembali setelah diperbaiki. Sistem sudah berjalan normal kembali~! yaay! <:1_CelLovely:797873134375796776>")
+
+    channel = client.get_channel(858697952436158464)
+    await channel.send(f"**{client.user}** telah diaktifkan kembali setelah diperbaiki. Sistem sudah berjalan normal kembali~! yaay! <:1_CelLovely:797873134375796776>")
+   
+client.run("bot ID")
