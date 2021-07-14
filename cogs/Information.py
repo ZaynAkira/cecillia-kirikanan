2947aaf6f8286abe52d232f9a954226ded7c1832
ZaynAkira <87105213+ZaynAkira@users.noreply.github.com>
2021-07-14 13:00:39 +0700

    Update Information.py

--- cogs/Information.py
+++ cogs/Information.py
@@ -133,7 +133,210 @@ class Information(commands.Cog):
         tanagoyang.add_field(name="Magnitudo", value=f"{magnitudo}\n\nJangan panik, tetap pantau keadaan terkini, dan waspada terhadap gempa susulan yang mungkin bisa saja terjadi.", inline=False)
         await ctx.send(embed=tanagoyang)
         
-     
+    #emoji info
+    @commands.command(aliases=["emote", "em"])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def emoji(self, ctx:Context, emoji:discord.PartialEmoji):
+        embed = discord.Embed(
+            title = "Emote Info",
+            color = ctx.guild.me.color
+        )
+        embed.add_field(name="Name", value=f"[:{emoji.name}:]({emoji.url})")
+        if emoji.animated is True:
+            embed.add_field(name="Animated?", value="Yes")
+        else:
+            embed.add_field(name="Animated??", value="No")
+        embed.add_field(name="ID", value=emoji.id)
+
+        embed.add_field(name="Date Added", value=emoji.created_at.strftime("%d-%m-%Y | %H:%M:%S"))
+        embed.add_field(name="ID Code", value=f"`{emoji.name}:{emoji.id}`")
+        if emoji.is_custom_emoji() is True:
+            embed.add_field(name="Custom Emote?", value="Yes")
+        else:
+            embed.add_field(name="Custom Emote?", value="No")
+
+        embed.set_image(url=emoji.url)
+
+        await ctx.send(embed=embed)
        
+    #emoji enlarger
+    @commands.command(aliases=["e", "enlarge", "en"])
+    async def enlarge(self, ctx, emoji:discord.PartialEmoji=None):
+        if not emoji:
+            return await ctx.reply("**Invalid Syntax!** Please write what emote you want to enlarge!!\nExample: `c.e :1_CelLovely:`")
+
+        link_emoji = emoji.url
+
+        #donlot emojinya
+        donlot = requests.get(link_emoji)
+        file = open("resize.png", "wb")
+        file.write(donlot.content)
+        file.close()
+
+        #kode utk me-resize emoji (untuk png saja)
+        def pembesar_gambar(namafile:str):
+            gambar = Image.open(namafile)
+            atur_ukuran = gambar.resize((1000, 1000))
+            atur_ukuran.save("resized.png")
+
+        #resize emoji-nya
+        async with ctx.typing():
+            if str(link_emoji).endswith(".gif"):
+                emubeddo = discord.Embed(
+                    title = "Emote Enlarger",
+                    description = "Unfotunately, `.gif` emote can't be resized same as `.png` emote",
+                    color = ctx.guild.me.color
+                )
+                emubeddo.set_image(url=link_emoji)
+                emubeddo.set_footer(text=f":{emoji.name}:")
+                await ctx.send(embed=emubeddo)
+            elif str(link_emoji).endswith(".png"):
+                pembesar_gambar("resize.png")
+
+                fairu = discord.File("./resized.png", filename="resized.png")
+                emubeddo = discord.Embed(
+                    title = "Emote Enlarger",
+                    color = ctx.guild.me.color
+                )
+                emubeddo.set_image(url=f"attachment://resized.png")
+                emubeddo.set_footer(text=f":{emoji.name}:")
+                await ctx.send(file=fairu, embed=emubeddo)
+                os.remove("resized.png")
+                os.remove("resize.png")
+
+    @enlarge.error
+    async def on_enlarge_error(self, ctx, error):
+        if isinstance(error, commands.CommandError):
+            await ctx.reply("Unfortunately, bot can't enlarge basic emote")
+            
+    #Spotify
+     @commands.command()
+    async def spotify(self, ctx, orang:discord.Member=None):
+        orang = ctx.author if not orang else orang
+
+        crotify = next((activity for activity in orang.activities if isinstance(activity, discord.Spotify)), None)
+
+        if crotify is None:
+            if orang == ctx.author:
+                embed = discord.Embed(
+                    description = f"You're not listening to Spotify currently.",
+                    color = 0xff0000
+                )
+                return await ctx.reply(embed=embed)
+            else:
+                embed = discord.Embed(
+                    description = f"{orang.mention} not listening to Spotify currently.",
+                    color = 0xff0000
+                )
+                return await ctx.reply(embed=embed)
+
+        embed = discord.Embed(
+            title= f"Listened by {orang.name} now",
+            color= crotify.color
+        )
+        embed.set_author(name="Spotify", icon_url="https://user-images.githubusercontent.com/75367930/120773067-293cc680-c54b-11eb-950e-a4518c464124.png")
+        embed.add_field(name="Title", value=f"[{crotify.title}](https://open.spotify.com/track/{crotify.track_id})")
+        embed.add_field(name="Artist", value=crotify.artist)
+        embed.add_field(name="Album", value=crotify.album)
+
+        waktusekarang = datetime.datetime.utcnow() - crotify.start
+        durasilagu = crotify.duration
+
+        embed.set_footer(text=f"Seek: {str(waktusekarang)[:-7]} / {str(durasilagu)[:-7]}", icon_url=orang.avatar_url)
+        #
+        try:
+            embed.set_thumbnail(url=crotify.album_cover_url)
+        except:
+            pass
+        #
+        await ctx.send(embed=embed)
+    
+    #KBBI
+    @commands.command()
+    @commands.cooldown(1, 7, commands.BucketType.user)
+    async def kbbi(self, ctx, *kata):
+        try:
+            if not kata:
+                await ctx.reply("<:robin_palato:818892964457349220> **Sintaks tidak valid!** Masukkan kata yang ingin kamu lihat di KBBI!\nContoh: `r!kbbi bantal`")
+            else:
+                async with ctx.typing():
+                    khata = "%20".join(kata)
+                    api_panas = requests.get(f"https://kbbi-api-zhirrr.vercel.app/api/kbbi?text={khata}").json()
+                    katah = api_panas["lema"]
+                    arti = api_panas["arti"][0]
+
+                    tabera = discord.Embed(
+                        title = katah.upper(),
+                        description = arti,
+                        color = ctx.guild.get_member(self.client.user.id).color
+                    )
+                    tabera.set_footer(text='Diberayakan oleh Badan Pengembangan dan Pembinaan Bahasa, Kemendikbud RI', icon_url=ctx.author.avatar_url)
+                    await ctx.send(embed=tabera)
+        except:
+            kahta = " ".join(kata)
+            embed = discord.Embed(
+                title = f"{kahta.upper()}",
+                description = "Kata tidak ditemukan...",
+                color = 0xff0000
+            )
+            embed.set_footer(text='Diberayakan oleh Badan Pengembangan dan Pembinaan Bahasa, Kemendikbud RI', icon_url=ctx.author.avatar_url)
+            await ctx.reply(embed=embed)
+            
+    #Role Info
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def roleinfo(self, ctx:commands.Context, *, role:discord.Role=None):
+        if role is None:
+            return await ctx.send("<:robin_palato:818892964457349220> **Sintaks tidak valid**! Tuliskan role yang ingin kamu lihat infonya!\nContoh: `r!roleinfo [role]`")
+
+        embed = discord.Embed(
+            title = f"Informasi Role",
+            color = role.color
+        )
+        embed.add_field(name="Nama", value=f"{role.mention} (`{role.name}`)")
+        embed.add_field(name="ID", value=role.id)
+        embed.add_field(name="Warna", value=str(role.color).upper())
+
+        if role.hoist is True:
+            embed.add_field(name="Terpisah", value="✅")
+        else:
+            embed.add_field(name="Terpisah", value="❌")
+        #
+        if role.mentionable is True:
+            embed.add_field(name="Bisa di-Mention", value="✅")
+        else:
+            embed.add_field(name="Bisa di-Mention", value="❌")
+        #
+        if role.managed is True:
+            embed.add_field(name="Di-Manage", value="✅")
+        else:
+            embed.add_field(name="Di-Manage", value="❌")
+
+        if role.is_default():
+            embed.add_field(name="Role Default", value="✅")
+        else:
+            embed.add_field(name="Role Default", value="❌")
+        #
+        if role.is_premium_subscriber():
+            embed.add_field(name="Role Booster", value="✅")
+        else:
+            embed.add_field(name="Role Booster", value="❌")
+        #
+        if role.is_bot_managed():
+            embed.add_field(name="Diatur oleh Bot", value="✅")
+        else:
+            embed.add_field(name="Diatur oleh Bot", value="❌")
+
+        embed.add_field(name="Posisi", value=role.position)
+        embed.add_field(name="Dibuat pada", value=role.created_at.strftime("%d/%m/%Y %H:%M:%S UTC"))
+        embed.add_field(name="Jumlah Member", value=len(role.members))
+
+        if sum(len(izin) for izin in [perm[0] for perm in role.permissions if perm[1]]) < 1024:
+            embed.add_field(name=f"Izin-Izin ({len([perm[0] for perm in role.permissions if perm[1]])})", value=f'`{", ".join([perm[0] for perm in role.permissions if perm[1]]).replace("_", " ")}`', inline=False)
+        else:
+            embed.add_field(name=f"Izin-Izin ({len([perm[0] for perm in role.permissions if perm[1]])})", value='Tidak muat', inline=False)
+
+        await ctx.send(embed=embed)    
+    
 def setup(client):
     client.add_cog(Information(client))
