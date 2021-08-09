6f1befa694bf13c094eca86431148d9033b0665a
ZaynAkira <87105213+ZaynAkira@users.noreply.github.com>
2021-08-09 17:54:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,5 @@ for namafile in os.listdir('./cogs'):
     if namafile.endswith('.py'):
         client.load_extension(f'cogs.{namafile[:-3]}')
         print(f"{namafile} dimuat")
-
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('c.help'))
-    print(f"\n{client.user} sudah siap.")
-
-    channel = client.get_channel(847885141019983962)
-    await channel.send(f"**{client.user}** telah diaktifkan kembali setelah diperbaiki. Sistem sudah berjalan normal kembali~! yaay! <:1_CelLovely:797873134375796776>")
-
-    channel = client.get_channel(858697952436158464)
-    await channel.send(f"**{client.user}** telah diaktifkan kembali setelah diperbaiki. Sistem sudah berjalan normal kembali~! yaay! <:1_CelLovely:797873134375796776>")
-   
+  
 client.run("ODY0MDQzNzIwMjk3OTM4OTQ2.YOvtbA.Aj9A1NGbr33GVvPqWPiUe2Qkioo")
