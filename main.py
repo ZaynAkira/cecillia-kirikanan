cd6afe9b2cc660ac0248bf79996b9c67106f79eb
ZaynAkira <87105213+ZaynAkira@users.noreply.github.com>
2021-10-06 07:57:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discord, os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix=["c.", "C."], case_insensitive=True, intents=discord.Intents.all())
+client = commands.Bot(command_prefix=["c.", "C.", "cel ", "Cel ", "Cecillia ", "cecillia "], case_insensitive=True, intents=discord.Intents.all())
 
 for namafile in os.listdir('./cogs'):
     if namafile.endswith('.py'):
         client.load_extension(f'cogs.{namafile[:-3]}')
-        print(f"{namafile} dimuat")
+        print(f"{namafile} loaded successfully")
+        
+@client.event
+async def on_ready():
+    print(f"\n{client.user} ready.")
   
 client.run("ODY0MDQzNzIwMjk3OTM4OTQ2.YOvtbA.Aj9A1NGbr33GVvPqWPiUe2Qkioo")
