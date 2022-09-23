36f554fb6332f0fa658a610458035a0ea15293db
Izuki Kurama <87105213+ZaynAkira@users.noreply.github.com>
2022-09-23 08:09:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
 import discord, os
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
+import asyncio
 
-client = commands.Bot(command_prefix=["c.", "C.", "cel ", "Cel ", "Cecillia ", "cecillia "], case_insensitive=True, intents=discord.Intents.all())
 
-for namafile in os.listdir('./cogs'):
-    if namafile.endswith('.py'):
-        client.load_extension(f'cogs.{namafile[:-3]}')
-        print(f"{namafile} loaded successfully")
-        
+TOKEN = #Tulis Token Anda disini
+#===============================================================================================
+
+#setel prefix + bikin variabel client
+client = commands.Bot(command_prefix=["c.", "C.", "cel ", "Cel ", "Cecillia ", "cecillia ","Cecillia", "cecillia" ], case_insensitive=True, intents=discord.Intents.all())
+
+#hilangkan help bawaan
+client.remove_command('help')
+
+#===============================================================================================
+
+#daftarin cog
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            # cut off the .py from the file name
+            await client.load_extension(f"cogs.{filename[:-3]}")
+
+#===============================================================================================
+
+#tulisan siap pada terminal dan status yg berganti2
 @client.event
 async def on_ready():
-    print(f"\n{client.user} ready.")
-  
-client.run("tebak")
+    change_status.start()
+    print('main.py, siap sedia!')
+    
+    channel = client.get_channel(791704672351813634)
+    await channel.send(f"{client.user} siap menerima perintah, Senpai!")
+    
+#===============================================================================================
+
+#isi status
+status = cycle([
+    'c.help',
+    "Join Chrono Curses!",
+    "With you~",
+    " ",
+    'Don\'t forget to wash your hand',
+    'Keep your distance with another people',
+    'avoid too much contact with random people',
+    'Stay safe',
+    "Enjoy your life whatever happened",
+    "Don't worry, Be Happy!"
+])
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+#===============================================================================================
+
+#nyalakan bot. 
+async def main():
+    async with client:
+        await load_extensions()
+        await client.start(TOKEN)
+
+asyncio.run(main())
